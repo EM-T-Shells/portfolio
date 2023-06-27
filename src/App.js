eead2c6027b18e08a9e4a36dbe52b10b41af52da
levius_codeWell <taskaleg@gmail.com>
2023-06-27 18:49:18 -0500

    updated prp

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
-// import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Testimonials from "./Components/Testimonials";
 import Portfolio from "./Components/Portfolio";
@@ -25,7 +24,6 @@ const App = () => {
     <div className="App">
       <Header data={resumeData.main} />
       <About data={resumeData.main} />
-      {/* <Resume data={resumeData.resume} /> */}
       <Portfolio data={resumeData.portfolio} />
       <Testimonials data={resumeData.testimonials} />
       <Contact data={resumeData.main} />
